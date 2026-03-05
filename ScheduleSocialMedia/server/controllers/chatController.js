53df2bf18aef6e850ee291718ff888449f5ec816
vuongthecuong99 <vuongthecuong99@g.ucla.edu>
2026-03-05 01:42:53 -0800

    Cleaing Unread Messages and fix some displaying issues

--- ScheduleSocialMedia/server/controllers/chatController.js
+++ ScheduleSocialMedia/server/controllers/chatController.js
@@ -27,25 +27,14 @@ router.get('/get-all-chats', authMiddleware, async (req, res) => {
     try{
         const chats = await Chat.find({ members: { $in: req.userId } })
             .populate("members")
+            .populate("lastMessage")
             .sort({ updatedAt: -1 });
-
-        const allChats = await Promise.all(
-         chats.map(async (chat) => {
-            const lastMessage = await Message.findOne({ chatId: chat._id })
-            .sort({ createdAt: -1 });
-
-            return {
-            ...chat._doc,
-            lastMessage,
-            };  
-         })
-       );
 
         res.status(200).send({
             message: "Chat fetched successfully",
             success: true,
-            data: allChats
-        })
+            data: chats
+        });
     }catch(error){
         res.status(400).send({
             message: error.message,
@@ -54,4 +43,39 @@ router.get('/get-all-chats', authMiddleware, async (req, res) => {
     }
 })
 
+router.post('/clear-unread-message', authMiddleware, async (req, res) => {
+    try{
+       const chatId = req.body.chatId;
+       
+       const chat = await Chat.findById(chatId);
+         if(!chat){
+             res.send({
+                 message: "Chat not found with given ID.",
+                 success: false
+             });
+         }
+        const updatedChat = await Chat.findByIdAndUpdate(
+            chatId, 
+            {unreadMessageCount: 0}, 
+            {new: true}
+        ).populate('members').populate('lastMessage');
+
+        await Message.updateMany(
+            { chatId: chatId, read: false},
+            { read: true }
+        )
+        
+        res.send({
+            message: "Unread message count cleared successfully.",
+            success: true,
+            data: updatedChat
+        })
+
+    }catch(error){
+        res.send({
+            message: error.message,
+            success: false
+        })
+    }
+})   
 module.exports = router;
