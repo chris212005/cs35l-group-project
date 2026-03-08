5c87cdb6dc97950980f8d60d072b7801521849e0
Christopher Rodriguez <christophercordova2332@gmail.com>
2026-03-08 16:39:57 -0700

    fix merge conflict in apiCalls index

--- ScheduleSocialMedia/src/apiCalls/index.js
+++ ScheduleSocialMedia/src/apiCalls/index.js
@@ -1,27 +1,23 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-export const url = "http://localhost:3000";
-=======
 export const url =
-  import.meta.env.VITE_API_URL || "http://localhost:3000";
->>>>>>> f3ff4f5 (use render backend url for frontend)
+  import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const axiosInstance = axios.create();
 
 axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+  (config) => {
+    const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.authorization = `Bearer ${token}`;
-    } else {
-      delete config.headers.authorization;
-    }
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
+    } else {
+      delete config.headers.authorization;
+    }
 
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
